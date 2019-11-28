d68145379992d3134eb3ab2960628862bef53f9a
Elkin de Armas <elkindearmas2015@gmail.com>
2019-11-27 21:24:03 -0500

    Plantilla Admin LTE instalada

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,22 @@
+@extends("theme.$theme.layout")
+
+@section('styles')
+
+@endsection
+
+@section('title')
+    Inicio
+@endsection
+
+@section('content')
+    <div class="row">
+
+
+
+    </div>
+
+@endsection
+
+@section('scripts')
+@endsection
+
